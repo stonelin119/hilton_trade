e9fa14809b003119c323ae28d9f6ac77e2c34ed2
linfeng <stonelin119@gmail.com>
2013-04-03 18:48:24 +0800

    config nginx

--- hilton_trade/settings.py
+++ hilton_trade/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = 'http://192.168.1.177/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/media/hilton_trade/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
